f0eb4a5723f3ca55aa92012d055066d3d7555aba
root <root@SNIXPUPPET>
2015-05-21 15:10:07 +0200

    Init Commit PuppetMaster

---
+++ environments/commun/modules/network/manifests/init.pp
@@ -0,0 +1,8 @@
+class network {
+ file { "/etc/resolv.conf":
+  owner => root,
+  group => root,
+  mode => 644,
+  source => "puppet:///modules/network/resolv.conf"
+ }
+}
---
+++ environments/developpement/manifests/site.pp
@@ -0,0 +1,23 @@
+#Ajouter ici les noeuds a gerer par le PuppetMaster
+node 'stestsles11.ima.intra' {
+	include repos
+	include motd
+	include ntp
+}
+
+node 'stestsles02.ima.intra' {
+	include repos
+	include motd
+}
+
+
+node 'stestrhel52.ima.intra' {
+	include repos
+	include ntp
+}
+
+
+node 'stestrhel64.ima.intra' {
+	include repos
+	include ntp
+}
